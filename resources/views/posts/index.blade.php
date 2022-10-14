722858f1f53ccea2491fbe27d06517c4fe9ba907
koya-namba <namba.koya@arakawa-lab.com>
2022-10-14 16:01:58 +0900

    create create and store function

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <div class='create'><a href='/posts/create'>create</a></div>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
