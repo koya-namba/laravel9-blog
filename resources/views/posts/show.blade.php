cfdd21662d8bb9ae4749cb701f2614087144211d
koya-namba <namba.koya@arakawa-lab.com>
2022-10-14 19:46:48 +0900

    create breeze

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,18 @@
-<!DOCTYPE HTML>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
-            </div>
+<x-app-layout>
+    <x-slot name="header">
+        Blog Show
+    </x-slot>
+    <h1 class="title">
+        {{ $post->title }}
+    </h1>
+    <div class="content">
+        <div class="content__post">
+            <h3>本文</h3>
+            <p>{{ $post->body }}</p>    
         </div>
-        <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
-</html>
+    </div>
+    <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+    <div class="footer">
+        <a href="/">戻る</a>
+    </div>
+</x-app-layout>
